5a894df0419fee37892864b73252fff8fc37c6b3
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-07-21 09:27:40 -0600

    Mouse clicks will now update the bit map

--- main.c
+++ main.c
@@ -24,13 +24,43 @@ int main(int argc, char **argv){
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+	unsigned int render_bitmap[32] = {0};	
+
 	SDL_Event event;
-	while(1){
+	
+	state = draw;
+	bool run = true;
+	while(run){
 		while(SDL_PollEvent(&event) == 1){
-			if((unsigned int) event.type == (unsigned int) SDL_Quit){
-				return 1;
+			switch(event.type){
+				case SDL_QUIT:
+					run = false;
+					break;
+
+				case SDL_MOUSEBUTTONUP:
+					if(event.button.button == SDL_BUTTON_LEFT){
+						//Turns a bit on if off and vice versa
+						render_bitmap[event.button.y/32] ^= 1 << (31 - (event.button.x/32));
+
+						/*	
+						//Debug bit mouse placement
+						for(int i = 0; i < 32; i++){
+							for(int j = 0; j < 32; j++){
+								printf("%d", (render_bitmap[i] >> (31 - j)) & 1);
+							}
+							printf("\n");
+					
+						}
+						printf("\n");
+						*/
+					}
 			}
-		}
+			if(run == false){
+				break;
+			}
+		}		
+
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_Quit();
--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 #define WINDOW_TITLE "John Conway's Game of Life"
 #define WINDOW_X 300
 #define WINDOW_Y 300
-#define WINDOW_W 600
-#define WINDOW_H 600
+#define WINDOW_W 640
+#define WINDOW_H 640
 #define WINDOW_FLAG 0
+
+enum {draw = 0, eval = 1} state;
